7e70a93a3cbf49e2e3553b41590aa39e1b02af47
Liudmila Kornilova <kornilova203@gmail.com>
2020-06-09 13:19:22 +0300

    v1.8.2

--- src/main/java/com/dbschema/mongo/MongoDatabaseMetaData.java
+++ src/main/java/com/dbschema/mongo/MongoDatabaseMetaData.java
@@ -447,7 +447,7 @@ public class MongoDatabaseMetaData implements DatabaseMetaData {
    * @see java.sql.DatabaseMetaData#getDriverVersion()
    */
   public String getDriverVersion() {
-    return "1.8.1";
+    return "1.8.2";
   }
 
   /**
