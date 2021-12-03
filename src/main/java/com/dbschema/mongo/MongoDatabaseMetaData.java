58a602e9103f7449f04fb1193f82addf932e18ec
Liudmila Kornilova <kornilova203@gmail.com>
2021-12-03 11:37:37 +0100

    v1.13

--- src/main/java/com/dbschema/mongo/MongoDatabaseMetaData.java
+++ src/main/java/com/dbschema/mongo/MongoDatabaseMetaData.java
@@ -446,7 +446,7 @@ public class MongoDatabaseMetaData implements DatabaseMetaData {
    * @see java.sql.DatabaseMetaData#getDriverVersion()
    */
   public String getDriverVersion() {
-    return "1.12";
+    return "1.13";
   }
 
   /**
@@ -460,7 +460,7 @@ public class MongoDatabaseMetaData implements DatabaseMetaData {
    * @see java.sql.DatabaseMetaData#getDriverMinorVersion()
    */
   public int getDriverMinorVersion() {
-    return 12;
+    return 13;
   }
 
   public boolean usesLocalFiles() {
